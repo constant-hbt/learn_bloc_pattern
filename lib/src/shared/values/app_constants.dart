ae396666225e4ab3acf5461a3d5c0c8e21e58d51
Henrique BT <henriqueborgesvotu@gmail.com>
2023-09-22 17:10:30 -0300

    :sprkles: feat: Atualização do arquivo de constantes

--- lib/src/shared/values/app_constants.dart
+++ lib/src/shared/values/app_constants.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: constant_identifier_names
 
 class AppConstants {
-  static const String SERVER_API_URL = 'http://192.168.0.103:8000/';
+  static const String SERVER_API_URL = 'http://192.168.0.190:8000/';
   static const String STORAGE_DEVICE_OPEN_FIRST_TIME = 'device_first_open';
   static const String STORAGE_USER_PROFILE_KEY = 'user_profile_key';
   static const String STORAGE_USER_TOKEN_KEY = 'user_token_key';
